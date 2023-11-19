17291d2dc6401c52721415a2d724afc6670e57c2
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2023-11-19 12:05:45 -0300

    feat: Adding the lesson 02

---
+++ 02-Visibility/example/example.go
@@ -0,0 +1,8 @@
+package example
+
+// Importing the fmt library
+import "fmt"
+
+func Printx() {
+	fmt.Printf("I printed from Printex")
+}
---
+++ 02-Visibility/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "./example"
+
+func main() {
+	// I Can only access functions from other package that have the a capital letter on the firt position of the world
+	example.Printx()
+}
